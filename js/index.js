a1bed46c527f8c1182d52e7906321509b2db9ee8
Joseph Camacho <spongkj@gmail.com>
2023-12-12 22:13:57 +0800

    test

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("Test");
+// const taas = prompt("Ano ang taas?");
+// const lapad = prompt("Ano ang lapad?");
+// const haba = prompt("Ano ang haba?");
+// const messa = 'Ang area na may taas na ${taas} at lapad na ${lapad}  ay :';
+// console.log(messa);
+
+const taas = 5;
+const lapad = 4;
+const haba = 3;
+const messa = "Ang area na may taas na " +taas+ "at lapad na " +lapad+ " ay :";
+console.log(messa);
