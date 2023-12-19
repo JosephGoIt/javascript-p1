ad0a02604fbec1da300eef679e29da965b283751
Joseph Camacho <spongkj@gmail.com>
2023-12-19 22:03:27 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,23 @@ const haba = 3;
 const messa = 'Ang area na may taas na  ${taas}  at lapad na  ${lapad}  ay : ';
 console.log(messa, taas * lapad);
 
+// const clients = ["Mango", "Banana", "Strawberry", "Cherry"];
+// const checkFruit = prompt("What fruit you are look for?");
+// for (let i=0; i<clients.length; i++){
+//     if(checkFruit === clients[i]) {
+//         console.log(`Fruit ${checkFruit} is available`);
+//         break;
+//     }
+//     else {console.log(`Fruit ${checkFruit} is not available`)}
+// }
+
+console.log("--------method-------");
+let message = "hello";
+console.log(message.split(""));
+
+const string = "Welcome to the future";
+let words = string.split("");
+console.log(words);
+for (let i=words.length - 1; i>=0; i--) {
+    console.log(words[i]);
+}
