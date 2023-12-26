189038e5785a51971858fe0ed9e872875ccac257
Joseph Camacho <spongkj@gmail.com>
2023-12-26 16:02:28 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,21 +33,51 @@ console.log("Test");
 // }
 
 
-function sum(nums) {
-    let total = 0;
-    for(let i = 0; i < nums.length; i++) {
-        total =+ nums[i];
-    }
-    return total;
-}
+// function sum(nums) {
+//     let total = 0;
+//     for(let i = 0; i < nums.length; i++) {
+//         total =+ nums[i];
+//     }
+//     return total;
+// }
 
-console.log(sum([5,6,7,8,9]));
+// console.log(sum([5,6,7,8,9]));
 
 
-function nFib(n) {
-    for (let i=0; i<n; i++) {
-        if (i>1){
-            valueToPrint = valueToPrint + 
-        }
-    }
-}
+// function nFib(n) {
+//     for (let i=0; i<n; i++) {
+//         if (i>1){
+//             valueToPrint = valueToPrint + 
+//         }
+//     }
+// }
+
+// function slugify(title) {
+//     // Change code below this line
+//     let newtitle;
+//     newtitle = title.toLowerCase();
+//     newtitle = newtitle.split(" ");
+//     newtitle = newtitle.join("-");
+//     return newtitle;
+//     // Change code above this line
+//   }
+
+// console.log(slugify('Arrays for begginers'));
+
+function makeArray(firstArray, secondArray, maxLength) {
+    // Change code below this line
+  let newArray;
+  newArray = firstArray.concat(secondArray);
+  if ((newArray.length-1) <= maxLength) {
+    return newArray;
+  }
+  return newArray.slice(0, maxLength);
+    // Change code above this line
+  }
+
+  console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+  console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+  console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
