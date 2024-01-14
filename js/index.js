32a4e90080f304716f966cf2cda5a9959344def3
Joseph Canacho <spongkj17@outlook.com>
2024-01-14 23:10:07 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -64,20 +64,20 @@ console.log("Test");
 
 // console.log(slugify('Arrays for begginers'));
 
-function makeArray(firstArray, secondArray, maxLength) {
-    // Change code below this line
-  let newArray;
-  newArray = firstArray.concat(secondArray);
-  if ((newArray.length-1) <= maxLength) {
-    return newArray;
-  }
-  return newArray.slice(0, maxLength);
-    // Change code above this line
-  }
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Change code below this line
+//   let newArray;
+//   newArray = firstArray.concat(secondArray);
+//   if ((newArray.length-1) <= maxLength) {
+//     return newArray;
+//   }
+//   return newArray.slice(0, maxLength);
+//     // Change code above this line
+//   }
 
-  console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-  console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-  console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+//   console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+//   console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+//   console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+//   console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+//   console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+//   console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
